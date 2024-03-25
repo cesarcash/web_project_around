1c928180fd01a1917bdf84dd03616502908e36fb
Cassini <cesarcash5@gmail.com>
2024-03-25 17:27:36 -0600

    añadir funcionalidad en post

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,51 +7,86 @@ const formPost = document.querySelector('#formNewPost');
 const formProfile = document.querySelector('#formEditProfile');
 const userName = document.querySelector('#userName');
 const userDescription = document.querySelector('#userDescription');
+const postContainer = document.querySelector('#post');
+const txtTitle = document.querySelector('#title');
+const txtUrl = document.querySelector('#url');
 
 const initialCards = [
-    {
-      name: "Valle de Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
-    },
-    {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
-    }
-  ];
-
-  function addPost(name,url){
-
-    const postContainer = document.querySelector('#post');
-    const postTemplate = document.querySelector('#postTemplate').content;
-    const postElement = postTemplate.querySelector('.post__item').cloneNode(true);
-
-    const postImage = postElement.querySelector('.post__image');
-    const postTitle = postElement.querySelector('.post__name');
-
-    postImage.setAttribute('src', url);
-    postImage.setAttribute('alt', name);
-
-    postTitle.textContent = name;
-
+  {
+    name: "Puerto progreso, Yucatán",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery__image1.jpg"
+  },
+  {
+    name: "La Habana, Cuba",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery__image7.jpg"
+  },
+  {
+    name: "Chiapas",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery_image5.jpg"
+  },
+  {
+    name: "Cartagena, Colombia",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery_image2.JPG"
+  },
+  {
+    name: "Oaxaca",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery__image3.jpg"
+  },
+  {
+    name: "Colonia, Uruguay",
+    link: "https://cesarcash.github.io/web_project_around/images/gallery__image4.jpg"
   }
+];
 
-formProfile.addEventListener('submit',sendFormProfile)
+initialCards.forEach((card) => {
+  addPost(card.name,card.link);
+})
+
+formProfile.addEventListener('submit',sendFormProfile);
+formPost.addEventListener('submit', createNewPost);
+window.addEventListener('load', function(){
+  
+  const btnLikes = document.querySelectorAll('.button_action_like');
+  
+  btnLikes.forEach((button) => {
+    button.addEventListener('click',handlePostLike)    
+  })
+
+})
+
+function handlePostLike(e){
+  const btnLiked = e.target;
+  btnLiked.classList.toggle('button_is_active');
+}
+
+function createNewPost(evt){
+
+  evt.preventDefault();
+
+  const titlePost = txtTitle.value;
+  const urlPost = txtUrl.value;
+
+  addPost(titlePost,urlPost);
+  this.reset();
+  closeModal();
+
+}
+
+function addPost(name,url){
+
+  const postTemplate = document.querySelector('#postTemplate').content;
+  const postElement = postTemplate.querySelector('.post__item').cloneNode(true);
+
+  const postImage = postElement.querySelector('.post__image');
+  const postTitle = postElement.querySelector('.post__name');
+
+  postImage.setAttribute('src', url);
+  postImage.setAttribute('alt', name);
+
+  postTitle.textContent = name;
+  postContainer.prepend(postElement);
+
+}
 
 btnClose.addEventListener('click', () => {
     closeModal()
@@ -64,7 +99,6 @@ btnEdit.addEventListener('click', function(){
     document.querySelector('#aboutMe').value = userDescription.textContent;
 })
 
-
 btnAdd.addEventListener('click', function(){
     handleOpenModal();
     formPost.style.display = 'block';
@@ -72,12 +106,6 @@ btnAdd.addEventListener('click', function(){
 
 function handleOpenModal(){
     modal.classList.add('popup_opened');
-}
-
-function closeModal(){
-    formProfile.style.display = 'none';
-    formPost.style.display = 'none';
-    modal.classList.remove('popup_opened')
 }
 
 function sendFormProfile(e){
@@ -91,4 +119,10 @@ function sendFormProfile(e){
 
     closeModal();
 
+}
+
+function closeModal(){
+  formProfile.style.display = 'none';
+  formPost.style.display = 'none';
+  modal.classList.remove('popup_opened')
 }
