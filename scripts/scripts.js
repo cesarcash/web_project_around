8d7dbcdc0e45e2e02196aa297b272e4785d5c10f
Cassini <cesarcash5@gmail.com>
2024-04-06 21:12:54 -0600

    validacion de formularios

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -154,3 +154,73 @@ function closeModal(){
   formPost.style.display = 'none';
   modal.classList.remove('popup_opened')
 }
+
+/** FORMS **/
+
+const showInputError = (formElement, inputElement, errorMessage) => { //ACTIVA ERROR
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => { //DESACTIVA ERROR
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => { //VALIDA INFORMACION INGRESADA
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => { //FALSO O VERDADERO
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => { //ACTIVA O DESHABILITA BOTON
+  console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("button_inactive");
+  } else {
+    buttonElement.classList.remove("button_inactive");
+  }
+};
+
+const setEventListeners = (formElement) => { //AÑADE ESCUCHA DE EVENTOS
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const buttonElement = formElement.querySelector('.form__button');
+  toggleButtonState(inputList,buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      toggleButtonState(inputList,buttonElement)
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+
+    // const fieldsetList = Array.from(formElement.querySelectorAll(".form__set")); // AÑADE LOS DIFERENTES FIELDSET
+
+    // fieldsetList.forEach((fieldset) => {
+    //   setEventListeners(fieldset);
+    // });
+  });
+};
+
+enableValidation();
