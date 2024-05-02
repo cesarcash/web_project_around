27c36875f0b1227642611d64bad7835683d28ff6
Cassini <cesarcash5@gmail.com>
2024-05-02 00:36:18 -0600

    creación de eventos

--- scripts/Card.js
+++ scripts/Card.js
@@ -10,13 +10,29 @@ export default class Card {
         return document.querySelector(this._cardSelector).content.querySelector('.post__item').cloneNode(true);
     }
 
-    _setEventListener(){
-        
+    _setEventListeners(){
+
+        this._element.querySelector('.button_action_like').addEventListener('click', () => {
+            this._handleLikeClick();
+        })
+
+        this._element.querySelector('.button_action_delete').addEventListener('click', (e) => {
+            const button = e.target;
+            button.closest('.post__item').remove();
+        })
+
+    }
+
+    _handleLikeClick(){
+
+        this._element.querySelector('.button_action_like').classList.toggle('button_is_active');
+
     }
 
     generateCard(){
 
         this._element = this._getTemplate();
+        this._setEventListeners();
         this._element.querySelector('.post__image').setAttribute('src',this._link);
         this._element.querySelector('.post__name').textContent = this._name;
         return this._element;
